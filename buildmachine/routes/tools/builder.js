edac6b0170d976e4bf4b359cc7eeae005e46383b
chunghuihaung <chunghui0904@gmail.com>
2015-12-23 14:50:41 +0800

    a

--- buildmachine/routes/tools/builder.js
+++ buildmachine/routes/tools/builder.js
@@ -16,7 +16,7 @@ BuildMachine.prototype.arduino = function (UploadPath, src, tmphex, callback){
 	var outputhex='main.hex';
 	var ARDCOREDIR = '/home/ubuntu/Build/buildmachine/build/hardware/arduino/cores/arduino'
 	//$ avr-gcc -g -DF_CPU=9600000 -Wall -L/usr/avr/lib -Os -mmcu=attiny13a -o tmp.elf tmp.o 
-	var cmdStr = "avr-gcc " + "-DF_CPU=" + D_CPU + "-lm -I/home/ubuntu/Build/buildmachine/build/hardware/arduino/cores/arduino -I$(" + ARDCOREDIR+ ")/../../variants/standard -mmcu=" + mcu + " -o " + path.join(tmppath, output) + " " + tmpdl;
+	var cmdStr = "avr-gcc " + "-DF_CPU=" + D_CPU + "-lm -I/home/ubuntu/Build/buildmachine/build/hardware/arduino/cores/arduino -I/home/ubuntu/Build/buildmachine/build/hardware/arduino/variants/standard -mmcu=" + mcu + " -o " + path.join(tmppath, output) + " " + tmpdl;
     var cmdHexStr = "avr-objcopy -O ihex -R .eeprom " + path.join(tmppath, output) +  " " +  path.join(tmppath, outputhex) ;
 	console.log("cmdStr: " + cmdStr);
 
