5929da87d219303b2c25dfe2b23798156500a7a5
chunghuihaung <chunghui0904@gmail.com>
2015-12-23 14:30:37 +0800

    a

--- buildmachine/routes/tools/builder.js
+++ buildmachine/routes/tools/builder.js
@@ -9,13 +9,14 @@ var BuildMachine = module.exports = function() {
 BuildMachine.prototype.arduino = function (UploadPath, src, tmphex, callback){
 	var tmppath = path.join(UploadPath, tmphex);
 	var tmpdl = path.join(tmppath, src);
-    console.log("tmppath: " + tmppath);
+
 	var D_CPU='16000000UL';
 	var mcu='atmega328p';
 	var output='main.o';
 	var outputhex='main.hex';
+	var ARDCOREDIR = '/home/ubuntu/Build/buildmachine/build/hardware/arduino/cores/arduino'
 	//$ avr-gcc -g -DF_CPU=9600000 -Wall -L/usr/avr/lib -Os -mmcu=attiny13a -o tmp.elf tmp.o 
-	var cmdStr = "avr-gcc " + "-DF_CPU=" + D_CPU + " -Wall -I/build/arduino/avr/cores/arduino -mmcu=" + mcu + " -o " + path.join(tmppath, output) + " " + tmpdl;
+	var cmdStr = "avr-gcc " + "-DF_CPU=" + D_CPU + "-lm -I$(ARDCOREDIR) -mmcu=" + mcu + " -o " + path.join(tmppath, output) + " " + tmpdl;
     var cmdHexStr = "avr-objcopy -O ihex -R .eeprom " + path.join(tmppath, output) +  " " +  path.join(tmppath, outputhex) ;
 	console.log("cmdStr: " + cmdStr);
 
@@ -39,7 +40,7 @@ BuildMachine.prototype.arduino = function (UploadPath, src, tmphex, callback){
 					console.log(cmdout);
 					console.log('child_process.exec is done. ');
 					if(typeof(callback)=='function'){
-						return callback(null, tmphex );
+						return callback(null, outputhex );
 					};
 				}
 			});
